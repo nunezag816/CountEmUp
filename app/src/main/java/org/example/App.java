f49af8fd8f380315e26d8e8b50ae2c594674d63e
nunezag816 <nunezag@msoe.edu>
2025-06-15 22:17:09 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,35 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        GroceryCounter counter = new GroceryCounter();
+
+        System.out.println(counter.total()); 
+
+        counter.tens();
+        counter.tens();
+        counter.hundreths();
+
+        System.out.println(counter.total());    
+        System.out.println(counter.overflows()); 
+
+        for (int i = 0; i < 35; i++) {
+            counter.ones();
+        }
+
+        System.out.println(counter.total());   
+        System.out.println(counter.overflows());
+
+        for (int i = 0; i < 100; i++) {
+            counter.ones();
+        }
+
+        System.out.println(counter.total());   
+        System.out.println(counter.overflows()); 
+
+        counter.clear();
+
+        System.out.println(counter.total());   
+        System.out.println(counter.overflows());
     }
 }
